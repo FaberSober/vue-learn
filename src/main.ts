1a145c511f013f90ff7c4ce3dd4ccec899eb5f4c
xupengfei <faberxu@gmail.com>
2024-03-21 15:44:49 +0800

    a

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createRouter, createWebHistory, DataLoaderPlugin } from 'vue-router/auto'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
+const router = createRouter({
+  history: createWebHistory(),
+  extendRoutes: (routes) => {
+    // routes.find((r) => r.name === '/')!.meta = {}
+    return routes
+  }
+})
+const pinia = createPinia()
+
+app.use(DataLoaderPlugin, { router })
 app.use(router)
+app.use(pinia)
 
 app.mount('#app')
