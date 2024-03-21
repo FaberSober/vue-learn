ac42157a8cfab4b01147097e6d9a1e1683c591d5
xupengfei <faberxu@gmail.com>
2024-03-21 15:47:25 +0800

    a

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,5 +11,14 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest'
-  }
+  },
+  rules: {
+    "import/no-default-export": "off",
+    "vue/multi-word-component-names": "off", //关闭组件命名规则
+    "vue/no-multiple-template-root": "off",
+    "no-console": "off",
+    // ignore配置vite配置的别名，临时办法
+    // "import/no-unresolved": [2, {"ignore": ["@/"]}],
+    // add specific rules configurations here
+  },
 }
